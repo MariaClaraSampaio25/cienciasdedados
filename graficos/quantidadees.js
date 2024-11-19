e029655adf7f3b9e67dc5aa5cc26658add69a2d9
Maria_Clara_Sampaio <127787008+MariaClaraSampaio25@users.noreply.github.com>
2024-11-19 23:37:14 +0000

    2

--- graficos/quantidadees.js
+++ graficos/quantidadees.js
@@ -1,4 +1,5 @@
-import { getCSS } from "./common.js"
+import { getCSS, tickConfig } from "./common.js"
+
 async function quantidadeEstudantes() {
   const url = 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/educacao/educacao-etapas-de-ensino.json'
   const res = await fetch(url)
@@ -15,11 +16,20 @@ async function quantidadeEstudantes() {
         }
       }
     ]
-    
+
     const layout = {
       plot_bgcolor: getCSS('--bg-color'),
-      paper_bgcolor: getCSS('--bg-color')
+      paper_bgcolor: getCSS('--bg-color'),
+      title: {
+         text: "Quantidade de Estudantes no Mundo",
+         font: {
+            color: getCSS('--primary-color'),
+            family: getCSS('--font'),
+            size: 30,
+         }
+      }
   }
+  
   const grafico = document.createElement('div')
   grafico.className = 'grafico'
   document.getElementById('graficos-container').appendChild(grafico)
